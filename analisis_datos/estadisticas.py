37941f9779dd883a1ef2f5b99646e452c2a5f6a0
Eric2175 <epadillamora@gmail.com>
2025-02-04 21:15:09 -0600

    actualizar
    actualizar

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,28 @@
 # estadisticas.py
+def media(datos):
+    """Esta función calcula la media aritmética con valores numéricos
+
+    Args:
+        datos (Lista): Lista de números
+
+    Returns:
+        Float
+    """
+    return sum(datos) / len(datos)
+
+def calcular_mediana(datos):
+    """Calcula la mediana
+
+    Args:
+        datos (Lista): Lista de números
+
+    Returns:
+        Float: da la mediana de una lista
+    """
+    datos_sorted = sorted(datos)
+    n = len(datos)
+    mid = n // 2
+    if n % 2 == 0:
+        return (datos_sorted[mid - 1] + datos_sorted[mid]) / 2.0
+    else:
+        return datos_sorted[mid]
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
+# Estudiante: Eric Padilla Mora
+# Fecha de Inicio: 2025/02/04
 # Fecha de Entrega: [dd/mm/aaaa]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+from analisis_datos.estadisticas import media,calcular_mediana
+
+lista_parametro = [5,3,1,2,4]
+
+print('Media:', media(lista_parametro))
+print('Mediana: ', calcular_mediana(lista_parametro))
